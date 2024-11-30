02a1df456ade0a8f38cc976cbec470557cb92d84
VatsBhavana <marathebhavana43@gmail.com>
2024-11-30 10:46:43 +0530

    Add files via upload

---
+++ Programming_style/lab.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program that demonstrates the correct use of indentation, comments, and
+variables following PEP 8 guidelines.
+"""
+n1 = int(input("Enter the value of number1: "))
+n2 = int(input("Enter the value of number2: "))
+
+# Multiplication of two variable answer store in result variable
+result = n1 + n2
+
+
+print(f"The addition of {n1} and {n2} is: {result}")
