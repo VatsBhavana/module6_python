0c7dbb730a920825abc6f27f365c98c42140960d
VatsBhavana <marathebhavana43@gmail.com>
2024-12-06 12:25:14 +0530

    Add files via upload

---
+++ core_python_concept/2c.py
@@ -0,0 +1,20 @@
+
+#votting 
+age=int("enter the age:")
+if age>=18:
+    print("you are eligible for votting.")
+else:
+    print("you are not eligible for votting.")
+
+    # '#' keyword use for single line comment
+    """
+    Triple quote shows multiline comment
+    """
+    """
+     input() is used to get input from the user.
+     int(input()) converts the input (a string) into an integer.
+     if checks if the condition (age >= 18) is true.
+     if true, it prints "You are eligible for voting."Otherwise, the else block runs.
+     Python is an interpreted language, which means it processes and executes code line by line.
+     """
+    
